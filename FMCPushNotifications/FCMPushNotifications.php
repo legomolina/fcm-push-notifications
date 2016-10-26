d8d78cca5e1c992dbc1dca27ff38d619ab9562f3
legomolina <legomolina@gmail.com>
2016-10-26 21:20:42 +0200

    Initial commit

---
+++ FMCPushNotifications/FCMPushNotifications.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace FCMPushNotifications;
+
+/**
+ * Class to send push notifications using Google Firebase
+ *
+ * NEEDS cURL module for PHP to work
+ *
+ * @author Cristian Molina
+ *
+ * Adapted from the code available at:
+ * https://github.com/mattg888/GCM-PHP-Server-Push-Message
+ *
+ * from the user Matt Grundy
+ */
+class FCMPushNotifications
+{
+    // Server API key obtained from Google
+    const API_KEY = 'my_api_key';
+
+    // Google URL to send the response
+    const URL = 'https://fcm.googleapis.com/fcm/send';
+
+    /**
+     * Sends a notification
+     * @param string $to Device token or topic to send the notification
+     * @param string $title The title to show in the notification
+     * @param string $message The message to show in the notification
+     * @param array $data Optional data to send
+     * @return mixed cURL response
+     */
+    public static function send($to, $title = "", $message = "", $data = array())
+    {
+        $fields = array(
+            "to" => $to,
+            "notification" => array(
+                "title"=> $title,
+                "text"=> $message
+            )
+        );
+
+        if (is_array($data) && count($data) > 0) {
+            foreach ($data as $key => $value) {
+                $fields['data'][$key] = $value;
+            }
+        }
+
+        $headers = array(
+            'Authorization: key=' . self::API_KEY,
+            'Content-Type: application/json'
+        );
+
+        // Open connection
+        $ch = curl_init();
+
+        // Set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, 'http://fcm.googleapis.com/fcm/send');
+
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+        // Avoids problem with https certificate
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        // Execute post
+        $result = curl_exec($ch);
+
+        // Close connection
+        curl_close($ch);
+
+        return $result;
+    }
+}
